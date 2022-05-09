b94575977b26294f04a9298cb334d74d8e375bf5
Rodrigo Curiel <rcuriel@artedigital-mx.com>
2022-05-09 18:49:40 -0500

    Add all files

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { ThemeProvider } from "styled-components";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./queries";
+import { Provider } from "react-redux";
+import configureStore from "./store/store";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
+const store = configureStore();
+
+function App() {
+  // specify base href from env variable 'PUBLIC_URL'
+  // use only if application isn't served from the root
+  // for development it is forced to root only
+  const basename = process.env.NODE_ENV === "development" ? "/" : "/";
+
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <ApolloProvider client={client}>
+          <BrowserRouter basename={basename}>
+            <Routes />
+          </BrowserRouter>
+        </ApolloProvider>
+      </ThemeProvider>
+    </Provider>
+  );
+}
+
+export default App;
